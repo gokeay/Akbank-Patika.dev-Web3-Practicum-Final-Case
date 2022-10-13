7b3e363ee0810d851fa406587f17bb61ba19c5ee
gokeay <83103331+gokeay@users.noreply.github.com>
2022-10-13 23:32:13 +0300

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,15 +1,10 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.2;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
 contract CARToken is ERC20 {
-
     constructor() ERC20("Car Token", "CRT") {
-
-        _mint(msg.sender, 100 * 10**uint(decimals()));
-
+        _mint(msg.sender, 100 * 10**18);
     }
-
 }
